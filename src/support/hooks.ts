534915f72ee38db2d91d2ba4e960681226609f8f
rishi-cd <rishibala.kanthavel@crystaldelta.com>
2025-08-11 15:11:44 +0530

    screen shot modification

--- src/support/hooks.ts
+++ src/support/hooks.ts
@@ -1,27 +1,33 @@
-import { BeforeAll, AfterAll, Before, After, setDefaultTimeout } from "@cucumber/cucumber";
+import {
+  BeforeAll,
+  AfterAll,
+  Before,
+  After,
+  setDefaultTimeout,
+} from "@cucumber/cucumber";
 import { chromium, Browser, BrowserContext, Page } from "@playwright/test";
 import randomstring from "randomstring";
 
 export let browser: Browser;
 export let context: BrowserContext;
 export let page: Page;
-setDefaultTimeout(120*1000*6)
+setDefaultTimeout(120 * 1000 * 6);
 
 BeforeAll(async function () {
-    browser = await chromium.launch({
-        headless: false,
-        args: ["--start-maximized", "--no-sandbox"]
-    });
-     context = await browser.newContext({
-        viewport: { width: 1000, height: 800 }
-    });
-    page = await context.newPage();
+  browser = await chromium.launch({
+    headless: false,
+    args: ["--start-maximized", "--no-sandbox"],
+  });
+  context = await browser.newContext({
+    viewport: { width: 1000, height: 800 },
+  });
+  page = await context.newPage();
 });
-After(async ({ error, pickle }) => {
-  if (error) {
+After(async ({ error, result, pickle }) => {
+  if (error || result.status === "FAILED") {
     console.log(error);
     await page.screenshot({
-      path: `asserts/screenshot${
+      path: `report-results/screenshots/${
         pickle.name
       }_${randomstring.generate()}.png`,
     });
@@ -29,6 +35,5 @@ After(async ({ error, pickle }) => {
 });
 
 AfterAll(async function () {
-    await browser.close();
-    
+  await browser.close();
 });
